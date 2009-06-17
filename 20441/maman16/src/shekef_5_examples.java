3285e774de17ef66e09f96d01996cd9df7e617a4
hq4ever <hq4ever@245f2a34-53fe-11de-833c-a7bd19eed978>
2009-06-17 19:03:41 +0000

    Recursion.

---
+++ 20441/maman16/src/shekef_5_examples.java
@@ -0,0 +1,16 @@
+
+public class shekef_5_examples {
+
+	public static boolean checkBiggerRec(int[] a, int i) {
+		int current = a[i];
+		
+		
+		
+		return checkBiggerRec(a, i-1);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
