5c771b819b17df00b231bec6c55a127a17ceb37a
hq4ever <hq4ever@245f2a34-53fe-11de-833c-a7bd19eed978>
2009-06-22 19:17:50 +0000

    Matrix solution, containing offsets in row,col positions.
    NOT a WORKING solution.

---
+++ trunk/20441/maman16/src/MatrixTest.java
@@ -0,0 +1,21 @@
+
+import org.junit.After;
+import org.junit.Before;
+
+
+public class MatrixTest {
+	Matrix matrix = new Matrix(new int[][] {
+			{-4, 2, 13, 22},
+			{-2, 5, 20, 24},
+			{5, 12, 25, 49},
+			{9, 13, 25, 57}});
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
