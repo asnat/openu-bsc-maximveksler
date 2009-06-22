92c8a3a515b746ebb5f78d82d82c570b044c4610
hq4ever <hq4ever@245f2a34-53fe-11de-833c-a7bd19eed978>
2009-06-22 19:17:50 +0000

    Matrix solution, containing offsets in row,col positions.
    NOT a WORKING solution.

---
+++ 20441/maman16/src/Matrix.java
@@ -0,0 +1,144 @@
+/**
+ * A matrix of integer numbers
+ */
+public class Matrix {
+ 
+    // The elements of the matrix
+    private int[][] _elements;
+
+    public boolean find(int x) {
+    	int n = getNumberOfRows();
+    	return find(x, n, n/2, 0, 0, n);
+    }
+
+    public boolean find(int x, 
+    		int checkpointRow, int checkpointCol, 
+    		int rowOffset, int colOffset, 
+    		int n) {
+    	
+    	int checkedValue = _elements[rowOffset+ checkpointRow-1][colOffset + checkpointRow-1];
+    	
+    	if(checkedValue == x)
+    		return true;
+    	else if(checkedValue > x){
+    		// Choose upper half
+    		find(x, 
+    				rowOffset + checkpointCol, colOffset + checkpointRow/2,
+    				rowOffset + n/2, colOffset,
+    				n/2);
+    	} else {
+    		// Choose lower half
+    		find(x, 
+    				rowOffset + checkpointCol, colOffset + checkpointRow/2,
+    				rowOffset, colOffset,
+    				n/2);
+    	}
+    	
+    	return false;
+    }
+    /**
+     * Constructs a new matrix from a table of values.
+     * @param elements a two dimentional array of integers
+     */
+    public Matrix(int[][] elements) {
+       
+        _elements = new int[elements.length][elements[0].length];
+        for (int i = 0; i <elements.length; i++){
+            for (int j = 0; j < elements[0].length; j++){
+                _elements[i][j] = elements[i][j];
+            }
+        }
+    }
+    
+    /**
+     * Constructs a new matrix with a given number of rows and columns.
+     * Constructs the matrix if rows and columns are positive numbers.
+     * @param rows a positive number
+     * @param columns a positive number
+     */
+    public Matrix(int rows, int columns){
+        if (rows>0&&columns>0)
+              _elements = new int[rows][columns];
+        
+    }
+    
+    /**
+     * 
+     * Returns the number of rows  the matrix has.
+     * @return the number of rows the matrix has
+     */
+    public int getNumberOfRows() {
+        return _elements.length;
+    }
+   
+    
+
+    /**
+     **Returns the number of columns the matrix has.   
+     * @return the number of columns the matrix has
+     */
+    public int getNumberOfColumns() {
+        return _elements[0].length;
+    }
+
+    /**
+     * Returns the value of a given index.
+     * If row or column not in matrix bounds 12345 is returned.
+     * @return the value of a given index or 12345 if indexes not valid
+     * @param row the row number
+     * @param column the column number
+     */
+    public int getElement(int row, int column){
+        if(isValid(row,column))
+              return _elements[row][column];
+        return 12345;      
+    }
+    
+    /**
+     * Returns the copy of this two dimentional array.
+     * @return reference to the copy of this two dimentional array
+     */
+    public int[][] getAllElements(){
+        int[][] copyElements =  new int[_elements.length][_elements[0].length];
+        for (int i = 0; i <_elements.length; i++){
+            for (int j = 0; j < _elements[0].length; j++){
+                copyElements[i][j] = _elements[i][j];
+            }
+        }
+        return copyElements;
+    }
+    // check if row and column valid
+    private boolean isValid(int row , int col)
+    {
+        return (((row>-1)&&(row<_elements.length)) &&
+                         ((col>-1)&&(col<_elements[0].length)));
+    }
+    /**
+     * Sets the value of a given index to a given value.
+     * If the row or column are in the matrix bounds the value is changed.
+     * @param row the row number
+     * @param column the column number
+     * @param value the new value
+     */
+    public void setElement(int row, int column, int value){
+        if(isValid(row,column))
+             _elements[row][column] = value;
+    }
+    
+    
+    /**
+     *Creates a string representation of the matrix.
+     * @return a String representation of this matrix
+     */
+    public String toString(){
+        String  buffer = "";
+        for (int i = 0; i < getNumberOfRows(); i++){
+            for (int j = 0; j < getNumberOfColumns(); j++){
+                buffer = buffer + getElement(i,j) + "\t";
+            }
+            buffer += "\n";
+        }
+        return buffer;          
+    }
+    
+}
