c4e17c8c09af960d638111192a6e5c98785a8494
hq4ever <hq4ever@245f2a34-53fe-11de-833c-a7bd19eed978>
2009-07-01 05:54:16 +0000

    Working WordNode.compareTo(WordNode node). Working WordNode.merge(WordNode) Working WordNode.split()
    Starting to finish implementation of merge sort.

--- 20441/maman17/src/WordNode.java
+++ 20441/maman17/src/WordNode.java
@@ -39,6 +39,19 @@ public class WordNode {
 		_occurrences = 1;
 	}
 
+	/**
+	 * Copy constructor with new next node.
+	 * 
+	 * @param node copied node
+	 * @param next WordNode representing the instance of WordNode that follows the current node in the list order.
+	 * 
+	 */
+	public WordNode(WordNode node, WordNode next) {
+		this._word = node._word;
+		this._occurrences = node._occurrences;
+		
+		this._next = next;
+	}
 	
 	// ###################
 	// ## PUBLIC      ####
@@ -87,20 +100,43 @@ public class WordNode {
 		
 	}
 	
+	private WordNode[] split() {
+		if(_next == null)
+			return new WordNode[] {this, null};
+		else {
+			WordNode[] nextSplit = _next.split();
+			return new WordNode[] {nextSplit[1], nextSplit[0]};
+		}
+	}
+	
 	private WordNode merge(WordNode node) {
-		 
 		if(node == null) // If we are asked to merge a linked list that is null we are finished.
 			return this; // Returning only this WordNode instance.
 		
-		// We have a _next node.
-		char nextChar = _next.getWordFirstChar();
-		char currentChar = getWordFirstChar();
+		int compareResult = compareTo(node);
 		
-		
+		if (compareResult == 0) {
+			// Note that if both nodes are equal we "lose" one node (delete it) and increment the counter of the second node.
+			_occurrences++;
+			return merge(node.getNext());
+		} else if (compareResult < 0) {
+			if (_next == null) {
+				// Current node word is smaller then node._word (should come before him).
+				// We have no tail so we simply add our self's before this node.
+				return new WordNode(this, node);
+			} else {
+				// We do have tail, so we need to merge the list into our tail
+				return new WordNode(this, _next.merge(node));
+			}
+		} else { // (compareResult > 0)
+			// We found a node that is bigger then us, so we return that node as current and call recursive merge to decide what it's
+			// tail will be with our value of the tail of the small node.
+			return new WordNode(node, merge(node.getNext()));
+		}
 	}
 	
 	/**
-	 * Compare between 2 words to decide they proper ordering in the list.
+	 * Compare between 2 words to decide their proper ordering in the list.
 	 * 
 	 * @return negative if word1 < word2, 0 if they are equal, positive if word1 > word2
 	 */
@@ -108,21 +144,26 @@ public class WordNode {
 		// Store the shorter string among the 2 supplied parameters
 		int shorterStringLength = (_word.length() > node.getWord().length()) ? node.getWord().length() : _word.length();
 
-		// First compare each char, stopping only when reaching end either string or find a char that is not equal.
+		// Compare each char
+		// 	Forced Stop condition: Reaching end of shortest string 
+		// 	Goal stop condition: Find a char that is not equal.
 		int i = 0;
-		while(!(i < shorterStringLength) && _word.charAt(i) == node.getWord().charAt(i)) 
-			i++;
-		
-		if(i == shorterStringLength -1) { 
-			// We have reached the end of one of the strings, meaning we now need to return a value.
-			if(_word.length() > node.getWord().length())
-				return 1;
-			else if (_word.length() < node.getWord().length())
-				return -1;
-		}
+		while(true) {
+			if(!(i < shorterStringLength)) {
+				// We have reached the end of one of the strings, meaning we now need to return a value.
+				if(_word.length() > node.getWord().length())
+					return 1;
+				else if (_word.length() < node.getWord().length())
+					return -1;
+				else
+					return 0;
+			}
+			
+			if( _word.charAt(i) != node.getWord().charAt(i))
+				return _word.charAt(i) - node.getWord().charAt(i);
 
-		// Else either strings are equal in length or we stopped at some unequal character before reaching end of string.
-		return _word.charAt(i) - node.getWord().charAt(i);
-		
-	}
-}
+			// If no failure condition has occured progress to next character.
+			i++;
+		}
+			
+	}}
