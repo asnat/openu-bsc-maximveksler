40a8c3b99100d3c52552baf6f6dc05d573565345
hq4ever <hq4ever@245f2a34-53fe-11de-833c-a7bd19eed978>
2009-07-31 08:01:23 +0000

    Maman 18, submission.

--- trunk/20441/manan18/src/StackSorter.java
+++ trunk/20441/manan18/src/StackSorter.java
@@ -5,7 +5,10 @@
  */
 public class StackSorter {
 	/**
-	 * Sorts the content of the supplied stack from lower to highest when the lowest element is at the head of the stack
+	 * Sorts the content of the supplied stack from lower to highest 
+	 * 	when the lowest element is at the head of the stack
+	 * 
+	 * This is an unstable sort.
 	 * 
 	 * @param s The Stack element upon which the sorting will be taking place.
 	 */
@@ -42,21 +45,62 @@ public class StackSorter {
 			s.push(sorted.pop());
 		}
 	}
-	
+
+	/**
+	 * Find and remove from supplied origin the smallest node value, 
+	 * 	the value is returned as plain int.
+	 *  
+	 * This operation is undefined if origin is null or does not contain values.
+	 * 
+	 * @param origin Stack that contains values upon which the search will be executed.
+	 * @return int the smallest value that has been found and removed from the stack.
+	 */
 	private static int removeSmallest(Stack origin) {
+		// tmp stack is used during the seeking phase.
 		Stack tmp = new Stack();
+		
+		// We start with the ultimate minimum in java
 		int min = Integer.MAX_VALUE;
 		
+		/*
+		 * Our removal algorithm is simple (and not very efficient, due to the nature of 
+		 * 	linked list based data structure implementations) :
+		 * 
+		 * We iterate over all values of origin, copying all values to a tmp stack and 
+		 * 	finding the smallest value.
+		 * 
+		 * Then we iterate again, this time over tmp copying all values into origin but this
+		 * 	time we make sure not to copy the smallest value that was found in the previous
+		 * 	iteration. 
+		 * 
+		 * The result of this 2 phase seek & destroy is: The smallest that was 
+		 * 	stored in origin is removed from the stack.
+		 */
+
+		// We want to continue copying as long as origin has values...
 		while(! origin.empty()) {
 			int val = origin.pop();
+			// Here we seek the smallest node, so update the value of min if it's current 
+			// 	value is bigger then val.
 			if(min > val)
 				min = val;
+			
+			// After the test is completed, store all checked values into a tmp stack.
 			tmp.push(val);
 		}
 		
+		// Marker flag, to suggest if a skip operation has happened for this run.
+		// 	We need this flag because we do not want to eliminate min values in a single scan.
 		boolean isSkipAllowed = true;
+
+		// We want to continue copying as long as tmp has values...
 		while(! tmp.empty()) {
 			int val = tmp.pop();
+			
+			// The actual removal, is we haven't skipped a value yet and current value
+			// of node matches our minimum then by all means skip it!
+			//
+			// In other words, Survival of the fittest... 
 			if(isSkipAllowed && val == min)
 				isSkipAllowed = false;
 			else
@@ -64,6 +108,7 @@ public class StackSorter {
 
 		}
 		
+		// Finally, we returned the minimum we found.
 		return min;
 	}
 }
