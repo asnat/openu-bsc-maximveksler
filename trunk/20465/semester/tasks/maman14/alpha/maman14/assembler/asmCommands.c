3c49ba97140c84c9dc36dbbf2baff9cd8c79bb44
hq4ever <hq4ever@245f2a34-53fe-11de-833c-a7bd19eed978>
2010-03-09 22:43:58 +0000

    Adapting assember command for addressing type and generic handling. Code compiles! YAY

--- trunk/20465/semester/tasks/maman14/alpha/maman14/assembler/asmCommands.c
+++ trunk/20465/semester/tasks/maman14/alpha/maman14/assembler/asmCommands.c
@@ -22,103 +22,49 @@ static unsigned short storeToCodeSegment(unsigned short dstRgstrCode,
     storeData(instruction);
 }
 
-short int twoArgumentCommand()
+short int commandTwoArguments(AsmInstruction asmInstruction,
+        unsigned short commandCode,
+        unsigned int supportedAddressing) {
+
+}
+
+short int commandOneArguments(AsmInstruction asmInstruction,
+        unsigned short commandCode,
+        unsigned int supportedAddressing) {
+
+}
+
+short int commandNulArguments(AsmInstruction asmInstruction,
+        unsigned short commandCode,
+        unsigned int supportedAddressing) {
+    
+}
 // #############################################################
 // ###### Assembly language implementation #####################
 // #############################################################
-/*void mov(AsmInstruction asmInst) {
-    short instructionCode = 0;
-    // verify has 2 operands.
-    // verify addressing methods are valid.
 
-    //storeToCodeSegment();
-}
-
-void cmp(AsmInstruction asmInst) {
-    short instructionCode = 1;
-}
-
-void add(AsmInstruction asmInst) {
-    short instructionCode = 2;
-}
-
-void sub(AsmInstruction asmInst) {
-    short instructionCode = 3;
-}
-
-void mul(AsmInstruction asmInst) {
-    short instructionCode = 4;
-}
-
-void div(AsmInstruction asmInst) {
-    short instructionCode = 5;
-}
-
-void lea(AsmInstruction asmInst) {
-    short instructionCode = 6;
-}
-
-void inc(AsmInstruction asmInst) {
-    short instructionCode = 7;
-}
-
-void dec(AsmInstruction asmInst) {
-    short instructionCode = 8;
-}
-
-void jmp(AsmInstruction asmInst) {
-    short instructionCode = 9;
-}
-
-void bne(AsmInstruction asmInst) {
-    short instructionCode = 10;
-}
-
-void red(AsmInstruction asmInst) {
-    short instructionCode = 11;
-}
-
-void prn(AsmInstruction asmInst) {
-    short instructionCode = 12;
-}
-
-void jsr(AsmInstruction asmInst) {
-    short instructionCode = 13;
-}
-
-void rts(AsmInstruction asmInst) {
-    short instructionCode = 14;
-}
-
-void hlt(AsmInstruction asmInst) {
-    short instructionCode = 15;
-}*/
-
-
-
-
-static conv_t cmdTable[] = {
-    {"mov", twoParm},
-    {"cmp", twoParm},
-    {"add", twoParm},
-    {"sub", twoParm},
-    {"mul", twoParm},
-    {"div", twoParm},
-    {"lea", twoParm},
-    {"inc", oneParm},
-    {"dec", oneParm},
-    {"jmp", oneParm},
-    {"bne", oneParm},
-    {"red", oneParm},
-    {"prn", oneParm},
-    {"jsr", oneParm},
-    {"rts", noParm},
-    {"hlt", noParm},
-    {NULL, NULL}
+static asm_cmd_struct cmdTable[] = {
+    {"mov", commandTwoArguments, 0, 0},
+    {"cmp", commandTwoArguments, 1, 0},
+    {"add", commandTwoArguments, 2, 0},
+    {"sub", commandTwoArguments, 3, 0},
+    {"mul", commandTwoArguments, 4, 0},
+    {"div", commandTwoArguments, 5, 0},
+    {"lea", commandTwoArguments, 6, 0},
+    {"inc", commandOneArguments, 7, 0},
+    {"dec", commandOneArguments, 8, 0},
+    {"jmp", commandOneArguments, 9, 0},
+    {"bne", commandOneArguments, 10, 0},
+    {"red", commandOneArguments, 11, 0},
+    {"prn", commandOneArguments, 12, 0},
+    {"jsr", commandOneArguments, 13, 0},
+    {"rts", commandNulArguments, 14, 0},
+    {"hlt", commandNulArguments, 15, 0},
+    {NULL, NULL, (int)NULL}
 };
 
 void process(AsmInstruction asmLineInstruction) {
-    conv_t *dynaFuncHandler;
+    asm_cmd_struct *dynaFuncHandler;
     char *handlerName;
 
     if(asmLineInstruction->instructionType == INST) {
@@ -135,7 +81,7 @@ void process(AsmInstruction asmLineInstruction) {
     /* If we have found a function at this name */
     if(dynaFuncHandler->function_name) {
         /* We want to pass the next location after the function name in the general user input string */
-        dynaFuncHandler->pt2func(asmLineInstruction);
+        dynaFuncHandler->pt2func(asmLineInstruction, dynaFuncHandler->commandCode, dynaFuncHandler->supportedAddressingBitmap);
     } else {
         handleError(asmLineInstruction->lineNumber, NO_SUCH_ASSEMBLY_COMMAND, handlerName, asmLineInstruction->_log_unparsedAssemblyLine);
 
