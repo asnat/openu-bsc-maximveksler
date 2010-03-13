66dc6e8c40a199a2d26699ec8f253c8add84febb
hq4ever <hq4ever@245f2a34-53fe-11de-833c-a7bd19eed978>
2010-03-13 12:35:40 +0000

    Private methods.

--- trunk/20465/semester/tasks/maman14/alpha/maman14/assembler/lineParser.c
+++ trunk/20465/semester/tasks/maman14/alpha/maman14/assembler/lineParser.c
@@ -18,3 +18,13 @@ AsmInstruction parseLine(char* line) {
     // in the end will returned parsed line.
     return 0;
 }
+
+void freeAsmInstruction(AsmInstruction asmInst) {
+    /*
+     * TODO: Free the whole asmInst memory model...
+     * this means go into each and every sub struct, sub union,
+     * see what was allocated on the heap and kick it from the
+     * memory table, otherwise you have memory leaks, which has
+     * direct corelation to the final grade.
+     */
+}
