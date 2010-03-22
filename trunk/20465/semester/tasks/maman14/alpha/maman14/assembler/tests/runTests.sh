c599a585ba883e661ad535a5b982f91190f6872d
hq4ever <hq4ever@245f2a34-53fe-11de-833c-a7bd19eed978>
2010-03-22 20:08:56 +0000

    Some initial tests

--- trunk/20465/semester/tasks/maman14/alpha/maman14/assembler/tests/runTests.sh
+++ trunk/20465/semester/tasks/maman14/alpha/maman14/assembler/tests/runTests.sh
@@ -8,12 +8,36 @@ fi
 
 VALID=expected.StdOut  # Update this file if you expect the program to output something that is not an error
 INVALID=expected.StdErr # Update this file if you expect the program to output something that is an error 
+ASM=../dist/Debug/GNU-Linux-x86/assembler
+ERROR_FOUND=false
 
 rm -f $VALID.real
 rm -f $INVALID.real
 
+exec 3>&1
+exec 4>&2
+
+exec 1>$VALID.real
+exec 2>$INVALID.real
+
+
+################################################################
+################################################################
+##### TESTS - add your assembly calls here... ##################
+################################################################
+$ASM NOSUCHFILE
+$ASM
+
+
+###################################
+# Post analysis
+###################################
+exec 1>&3
+exec 2>&4
+
 if ! diff $VALID $VALID.real; then
 	echo FUCK Build failed because expected output was unexpected
+	ERROR_FOUND=true
 fi
 
 echo
@@ -22,8 +46,16 @@ echo
 
 if ! diff $INVALID $INVALID.real; then
 	echo FUCK it, Build failed because unexpected output wasn\'t fun
+	ERROR_FOUND=true
 fi
 
 
+echo 
+echo
+if [[ $ERROR_FOUND = "false" ]]; then
+	echo "IT WORKED, it fucking worked. No one will ever believe you..."
+else 
+	echo "BUILD FAILED. Yup, you broke it... AGAIN!"
+fi
 
 
