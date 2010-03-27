cde2f25baad7c3a6fed5e5af4a61d1ab1166026a
doronunu <doronunu@245f2a34-53fe-11de-833c-a7bd19eed978>
2010-03-27 07:03:09 +0000

    just so ican work at Max's house

--- 20465/semester/tasks/maman14/alpha/maman14/assembler/outputFilesHandler.c
+++ 20465/semester/tasks/maman14/alpha/maman14/assembler/outputFilesHandler.c
@@ -3,6 +3,8 @@
 #include "errorHandler.h"
 #include "constants.h"
 #include "outpuFilesHandler.h"
+#include "codeSegmentMgr.h"
+/* #include dataSegmentMgr.h */
 
 #define OBJ_FILE_SUFFIX ".ob"
 #define EXT_FILE_SUFFIX ".ext"
@@ -26,6 +28,54 @@ static char* createFilePath(char* filePrefix,char* suffix,unsigned suffixSize, u
 
     return filePathPointer;
 }
+
+_bool writeToOutputFile(char* fileType, char* labelName, unsigned address){
+    FILE* fileName;
+    
+    switch (fileType){
+        case EXT_FILE:
+            if ((fileName = fopen(currentExtFile,"w+")) != NULL)
+                fprintf(currentExtFile,"%s\t0%u", labelName, address);
+            else {
+                perror(currentExtFile);
+                return FALSE;
+            }
+            break;
+        case ENT_FILE:
+            if ((fileName = fopen(currentEntFile,"w+")) != NULL)
+                fprintf(currentEntFile,"%s\t0%u", labelName, address);
+            else {
+                perror(currentEntFile);
+                return FALSE;
+            }
+            break;
+        default:
+            return FALSE;
+    }
+    if (fileType == EXT_FILE)
+        fclose(currentExtFile);
+    else
+        fclose(currentEntFile);
+    return TRUE;
+}
+
+ _bool writeObjectFileFirstRow(){
+     FILE* obFile;
+
+     if ((obFile = fopen(currentObjFile,"w")) == NULL){
+         perror(currentObjFile);
+         return FALSE;
+     }
+     fprintf(currentObjFile,"\t\t0%u 0%u",getIC,getID);
+     fclose(currentObjFile);
+     return TRUE;
+ }
+
+
+_bool writeToObjFile(unsigned address, unsigned short data, char linkerData){
+    fprintf
+}
+
 
 void initOutputFiles(char* filePath){
     unsigned fileNameSize;
---
+++ 20465/semester/tasks/maman14/alpha/maman14/assembler/phaseTwo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "constants.h"
+#include "codeSegmentMgr.h"
+/* #include dataSegmentMgr.h */
+#include "asmInstruction.h"
+#include "lineParser.h"
+#include "errorHandler.h"
+#include "outputFilesHandler.h"
+
+#define LINE_LENGTH 81
+
+_bool phaseTwo(FILE* currentFileHamdler){
+    char fc, line[LINE_LENGTH], linkerLineType[SEGMENT_MAXIMUM_SIZE];
+    int lineIndex=0;
+    AsmInstruction instructionLine;
+
+    
+    if(!writeObjectFileFirstRow())
+        fatalError(2,"moreInfo");
+    /* setIC(0);*/
+    /* setDC(0);*/
+
+    while ((fc = fgetc(currentFileHamdler)) != EOF){
+        if (fc != '\n'){
+            line[lineIndex++] = fc;
+        }
+        instructionLine = parseLine(line);
+        
+    }
+}
